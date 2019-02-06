7701baad54d0ddb44eb800e332809e9cd33ec843
Anastasia <nastyakos11@gmail.com>
2019-02-06 23:37:28 +0300

    done task1-2

--- functions/tasks/task1.js
+++ functions/tasks/task1.js
@@ -1,1 +1,21 @@
 // task1 description
+/* Создать функцию multiply, которая будет принимать любое количество чисел и возвращать их произведение:
+multiply(1, 2, 3) = 6
+Еслли нет ни одного аргумента, вернуть ноль: multiply() = 0
+*/
+
+function multiply() {
+    if ( arguments.length === 0) {
+       return 0;
+    }
+    let result = 1,
+        max = arguments.length;
+    for (let i = 0; i < max; i++) {
+        result *= arguments[i];
+    }
+    return result;
+
+}
+
+console.log (multiply(55, 0, 1, 25))
+
---
+++ functions/tasks/task2.js
@@ -0,0 +1,14 @@
+/* создать функцию, которая принимает строку и
+возвращает строку-перевертыш:
+reverseString('test')="tset"
+*/
+function reverseString(string) {
+   let result = '';
+   for (let i = 1; i <= string.length; i++) {
+      result += string[string.length - i];
+   }
+
+      return result;
+}
+
+console.log(reverseString('test'))
