7f4e10858ccfc5b6c68aa05eb700a80efac90e05
Anastasia <nastyakos11@gmail.com>
2019-02-08 01:03:10 +0300

    done task7-8

---
+++ functions/tasks/task7.js
@@ -0,0 +1,10 @@
+/*Задача «Hello World» для функций :)
+
+Напишите функцию min(a,b), которая возвращает меньшее из чисел a,b.
+*/
+
+function min(a, b) {
+     return (a < b) ? a : b
+}
+
+console.log(min (5, 2));
