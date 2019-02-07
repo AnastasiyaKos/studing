3e3d64c96373c85548bd8c673518f373c858eb96
Anastasia <nastyakos11@gmail.com>
2019-02-07 23:53:24 +0300

    done task6

---
+++ functions/tasks/task5.js
@@ -0,0 +1,12 @@
+/* создать 2 функции и дать им осмысленные названия:
+-первая функция принимает массив и колбэк
+-вторая функция(КОЛБЭК) орабатывает каждый элемент массива
+Первая функция возвращает строку "New value:" и обрабатывает массив:
+
+firstFunc(['my', 'name', 'is', 'Trinity'], secondFunk) - "New value: MyNamesIsTrinity"
+firstFunc([10, 20, 30], secondFunc) - "New value: 100, 200, 300"
+firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aron}], secondFunc) -
+   "New value: Jhon is 45, Aron is 20"
+firstFunc(['abc', '123'], secondFunc) - "New value: cba, 321"
+firstFunc([1, 2, 3], function(namber) {return number + 5 + ''}) - New value: 6, 7, 8
+ */
