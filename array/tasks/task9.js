6b9e07b24959446a19a15ee5d6f6056418ccaea3
Anastasia <nastyakos11@gmail.com>
2019-02-20 00:17:37 +0300

    commented 9 task

--- array/tasks/task9.js
+++ array/tasks/task9.js
@@ -2,12 +2,12 @@
 удалить из [1,2,3,4,5] второй и третий элементы
  */
 
-function delElem() {
-    const array = [];
-
-    for (let i = 0; i < array.length; i++){
-       if (i = ) {
-           
-       }
-    }
-}
+// function delElem() {
+//     const array = [];
+//
+//     for (let i = 0; i < array.length; i++){
+//        if (i = ) {
+//
+//        }
+//     }
+// }
