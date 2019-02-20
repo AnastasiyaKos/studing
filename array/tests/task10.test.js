face502aa1c77b9b5a0deea208b3cdf0ba20a13c
Anastasia <nastyakos11@gmail.com>
2019-02-20 23:43:04 +0300

    commented test

--- array/tests/task10.test.js
+++ array/tests/task10.test.js
@@ -1,3 +1,9 @@
 const assert = require('chai').assert;
-const {sort} = require('../tasks/task1');
+const {insert} = require('../tasks/task10');
 
+describe('#ARRAY TASK10 Insert element', () => {
+    it ('Should be: [ I, am, an, awesome, array]', (done) => {
+        assert.deepEqual(insert(['I', 'am', 'an', 'array'], 'awesome'),[ 'I', 'am', 'an', 'awesome', 'array' ]);
+        done()
+    });
+});
